eab8790893b7cc4378e19edd4c55cdbaeee1db9b
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-21 01:09:07 +0200

    Create and style footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
     <div className="Page">
       <Header />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="copyrights">
+        <h4>Living the Simple Life</h4>
+        <p>Copyright 2019</p>
+      </div>
+    </footer>
+  )
+}
