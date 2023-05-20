dc6d1d1c29cd18f8703ed05a9429008786c507bd
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-21 00:36:10 +0200

    Link fonts and css files and style title section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 export default function Header() {
   return (
     <header>
-      <div className="title-section">
-        <h1>Living the simple life</h1>
-        <p>A BLOG EXPLORING MINIMALISM IN LIFE</p>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">HOME</a>
-            <a href="#">ABOUT</a>
-            <a href="#">RECENT POSTS</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <div className="title-section">
+          <h1>Living the simple life</h1>
+          <p>A BLOG EXPLORING MINIMALISM IN LIFE</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">HOME</a>
+              <a href="#">ABOUT</a>
+              <a href="#">RECENT POSTS</a>
+            </li>
+          </ul>
+        </nav>
+      </div> 
     </header>
   )
 }
