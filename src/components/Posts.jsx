025bb4cf2c78cdab41061435b2a406bfd5667ab2
Milo <92605623+MiloBlackstar@users.noreply.github.com>
2023-05-21 04:03:44 +0200

    Create Post component and finish recent post bar

---
+++ src/components/Posts.jsx
@@ -0,0 +1,10 @@
+import Food from '../images/food.jpg'
+
+export default function Posts() {
+  return (
+    <div className="white-border">
+          <img className="right-section-images" src={Food} />
+          <p class="post-title">Keeping cooking simple</p>
+    </div>
+  )
+}
--- src/components/RecentPostsSmall.jsx
+++ src/components/RecentPostsSmall.jsx
@@ -1,13 +1,14 @@
-import Food from '../images/food.jpg'
+import Posts from './Posts.jsx'
 
 export default function RecentPostsSmall() {
   return (
     <div className="small-container">
       <div className="white-border">
-          <h3 className="article-title">RECENT POSTS</h3>
-          <img className="right-section-images" src={Food} />
-          <p class="post-title">Keeping cooking simple</p>
+        <h3 className="article-title">RECENT POSTS</h3>
       </div>
+      <Posts />
+      <Posts />
+      <Posts />
     </div>
   )
 }
